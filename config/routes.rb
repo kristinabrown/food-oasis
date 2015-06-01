7d382b98e93238761ebcb55b39c4a9cf84b9d829
David Daniel <davedan7@gmail.com>
2015-06-01 17:17:38 -0600

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   root 'staticpages#index'
+
+  resources :users
+  resources :bulletins
+  # resources :map_markers
   
   get 'find', to: 'staticpages#find'
   
   
   get 'learn-more', to: 'staticpages#learn_more'
-  
-  resources :map
   
   
 end
